f0bd098e6c9ed7d8725f906e4c5d86590af6e800
Shreed95 <116705041+Shreed95@users.noreply.github.com>
2023-12-03 13:38:31 +0530

    Modified Database Connection

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./db");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-dotenv.config()
+connectDB();
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => app.listen(process.env.PORT, () => console.log("Mongo DB Connected")))
-  .catch((error) => console.log(error.message));
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected");
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on('error', err =>{
+  console.log(err);
+})
